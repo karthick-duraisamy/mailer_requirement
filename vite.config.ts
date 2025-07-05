321edfcd0a9f6623ab59efc5c230ac928180621e
karthick.d <karthick.d@infinitisoftware.net>
2025-07-05 10:35:36 +0530

    Updated code changes for api integration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
